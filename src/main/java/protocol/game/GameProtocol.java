49d7c518a45c3be8480c498227ebb447490d0417
yechanpark <qkrdpcks0419@naver.com>
2019-04-05 00:13:12 +0900

    처형 찬성 투표 프로토콜 추가

--- src/main/java/protocol/game/GameProtocol.java
+++ src/main/java/protocol/game/GameProtocol.java
@@ -13,13 +13,14 @@ import protocol.game.subprotocol.*;
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
 @JsonSubTypes({
-        @JsonSubTypes.Type(value = JobActionProtocol.class,     name = "JOB_ACTION"),
-        @JsonSubTypes.Type(value = PhaseChangeProtocol.class,   name = "PHASE_CHANGE"),
-        @JsonSubTypes.Type(value = ResultProtocol.class,        name = "RESULT"),
-        @JsonSubTypes.Type(value = JobAllocationProtocol.class, name = "JOB_ALLOCATION"),
-        @JsonSubTypes.Type(value = UserSelectionProtocol.class, name = "USER_SELECTION"),
-        @JsonSubTypes.Type(value = DoctorHealProtocol.class,    name = "JOB_DOCTOR_HEAL"),
-        @JsonSubTypes.Type(value = UserDeadProtocol.class,      name = "USER_DEAD")
+        @JsonSubTypes.Type(value = JobActionProtocol.class,         name = "JOB_ACTION"),
+        @JsonSubTypes.Type(value = PhaseChangeProtocol.class,       name = "PHASE_CHANGE"),
+        @JsonSubTypes.Type(value = ResultProtocol.class,            name = "RESULT"),
+        @JsonSubTypes.Type(value = JobAllocationProtocol.class,     name = "JOB_ALLOCATION"),
+        @JsonSubTypes.Type(value = UserSelectionProtocol.class,     name = "USER_SELECTION"),
+        @JsonSubTypes.Type(value = DoctorHealProtocol.class,        name = "JOB_DOCTOR_HEAL"),
+        @JsonSubTypes.Type(value = UserDeadProtocol.class,          name = "USER_DEAD"),
+        @JsonSubTypes.Type(value = ExecuteVoteAgreeProtocol.class,  name = "VOTE_EXECUTE_AGREE")
 })
 public abstract class GameProtocol implements Protocol {
     @Override
